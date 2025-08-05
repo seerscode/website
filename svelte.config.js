8d432e3ce9f28e045770e4a4183fcb659c60c14b
Marcio <marcio@pense.dev>
2025-08-05 10:08:29 +0200

    Fix latex

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,21 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// svelte.config.js
 import { mdsvex } from 'mdsvex';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath   from 'remark-math';
+import rehypeKatex  from 'rehype-katex';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-  extensions: ['.md', '.svx'],
-  remarkPlugins: [remarkMath],
-  rehypePlugins: [rehypeKatex],
-};
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   extensions: ['.svelte', '.md', '.svx'],
 
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: [
-    mdsvex(mdsvexOptions),
+    mdsvex({
+      extensions: ['.md', '.svx'],
+      remarkPlugins: [remarkMath],    // 👈 will now be used
+      rehypePlugins: [rehypeKatex]    // 👈 will now be used
+    }),
     vitePreprocess()
   ],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: { adapter: adapter() }
 };
-
-export default config;
